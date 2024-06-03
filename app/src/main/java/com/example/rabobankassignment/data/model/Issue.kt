4c12ac1115d35eca2f76fd00cc1eb81c7ab1db41
Harun KARACA <harunkaraca99@gmail.com>
2024-06-03 13:12:29 +0200

    model implemented

---
+++ app/src/main/java/com/example/rabobankassignment/data/model/Issue.kt
@@ -0,0 +1,11 @@
+package com.example.rabobankassignment.data.model
+
+import java.util.Date
+
+data class Issue(
+    var firstName: String,
+    var surName: String,
+    var issueCount: Int,
+    var dateOfBirth: Date,
+    var avatarUrl: String
+)
