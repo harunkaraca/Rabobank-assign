23aadf4336a8b7936472bdce9958e0b187747b8a
Harun KARACA <harunkaraca99@gmail.com>
2024-06-04 17:13:14 +0200

    fake data added

--- app/src/androidTest/java/com/example/rabobankassignment/data/FakeRepository.kt
+++ app/src/androidTest/java/com/example/rabobankassignment/data/FakeRepository.kt
@@ -1,18 +1,21 @@
 package com.example.rabobankassignment.data
 
+import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.MutableLiveData
 import com.example.rabobankassignment.data.model.Issue
 import com.example.rabobankassignment.data.model.Result
+import kotlinx.coroutines.runBlocking
+import java.util.Date
 import java.util.LinkedHashMap
 import javax.inject.Inject
 
 class FakeRepository @Inject constructor():BaseRepository {
 
-    var serviceData: LinkedHashMap<String, Issue> = LinkedHashMap()
+    var datas: LinkedHashMap<String, Issue> = LinkedHashMap()
 
     private var shouldReturnError = false
 
-    private val observableTasks = MutableLiveData<Result<List<Issue>>>()
+    private val observableIssues = MutableLiveData<Result<List<Issue>>>()
 
     fun setReturnError(value: Boolean) {
         shouldReturnError = value
@@ -22,6 +25,18 @@ class FakeRepository @Inject constructor():BaseRepository {
         if (shouldReturnError) {
             return Result.Error(Exception("Test exception"))
         }
-        return Result.Success(serviceData.values.toList())
+        val issue1 = Issue("Name1", "Surname1",1, Date(),"https://api.multiavatar.com/2cdf5db9b4dee297b7.png")
+        val issue2 = Issue("Name2", "Surname2",1, Date(),"https://api.multiavatar.com/2cdf5db9b4dee297b7.png")
+        val issue3 = Issue("Name3", "Surname3",1, Date(),"https://api.multiavatar.com/2cdf5db9b4dee297b7.png")
+        datas[issue1.firstName]=issue1
+        datas[issue2.firstName]=issue2
+        datas[issue3.firstName]=issue3
+        return Result.Success(datas.values.toList())
+    }
+    private fun addIssues(vararg issues: Issue) {
+        for (task in issues) {
+            datas[task.firstName] = task
+        }
+        runBlocking { observableIssues.value=Result.Success(datas.values.toList()) }
     }
 }
