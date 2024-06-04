89b33177f43da7ed73478db51efb07f3415703df
Harun KARACA <harunkaraca99@gmail.com>
2024-06-04 21:02:11 +0200

    ui test implemented

--- app/src/androidTest/java/com/example/rabobankassignment/main/MainFragmentTest.kt
+++ app/src/androidTest/java/com/example/rabobankassignment/main/MainFragmentTest.kt
@@ -1,40 +1,32 @@
 package com.example.rabobankassignment.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ActivityScenario.ActivityAction
-import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.clearText
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.example.rabobankassignment.MainActivity
 import com.example.rabobankassignment.R
-import com.example.rabobankassignment.data.BaseRepository
-import com.example.rabobankassignment.data.FakeRepository
-import com.example.rabobankassignment.data.model.Issue
-import com.example.rabobankassignment.data.model.Result
 import com.example.rabobankassignment.launchFragmentInHiltContainer
 import com.example.rabobankassignment.util.EspressoIdlingResource
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runTest
 import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.Date
 import javax.inject.Inject
 
 
@@ -48,28 +40,50 @@ class MainFragmentTest{
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
-    @Inject
-    lateinit var repository: FakeRepository
-
     @Before
     fun init() {
         // Populate @Inject fields in test class
         hiltRule.inject()
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+    }
+
+    /**
+     * Unregister your Idling Resource so it can be garbage collected and does not leak any memory.
+     */
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+    }
+    @Test
+    fun wrongUrlShouldShowSnackBar(){
+        launchFragmentInHiltContainer<MainFragment>(null, R.style.Theme_RaboBankAssignment)
+        onView(withId(R.id.etUrl)).perform(clearText(), typeText("dka"));
+        onView(withId(R.id.btnDownload)).perform(click())
+        onView(withId(R.id.tvError)).check(matches((isDisplayed())))
+        onView(withId(com.google.android.material.R.id.snackbar_text)) .check(matches(withText("Illegal state")))
+    }
+    @Test
+    fun emptyUrlShouldShowSnackBar(){
+        launchFragmentInHiltContainer<MainFragment>(null, R.style.Theme_RaboBankAssignment)
+        onView(withId(R.id.etUrl)).perform(clearText(), typeText(""));
+        onView(withId(R.id.btnDownload)).perform(click())
+        onView(withId(com.google.android.material.R.id.snackbar_text)) .check(matches(withText(R.string.url_can_not_be_empty)))
     }
 
     @Test
-    fun noDownloadedFile(){
+    fun noDownloadedFileInvisibleRecyclerView(){
         launchFragmentInHiltContainer<MainFragment>(null, R.style.Theme_RaboBankAssignment)
         onView(withId(R.id.rcvList)).check(matches(not(isDisplayed())))
     }
-
     @Test
-    fun downloadFileCheckRecyclerViewHasItem(){
-        val result=runBlocking {
-            repository.downloadFile("https://raw.githubusercontent.com/RabobankDev/AssignmentCSV/main/issues.csv")
+    fun downloadFileCheckRecyclerViewHasItems(){
+        launchFragmentInHiltContainer<MainFragment>(null, R.style.Theme_RaboBankAssignment)
+        onView(withId(R.id.etUrl)).perform(clearText(), typeText("https://raw.githubusercontent.com/RabobankDev/AssignmentCSV/main/issues.csv"));
+        onView(withId(R.id.btnDownload)).perform(click())
+        runBlocking {
+            delay(1000L)
         }
-        launchFragmentInHiltContainer<MainFragment>(null, R.style.Theme_RaboBankAssignment)
-        (result as Result.Success)
-        onView(withId(R.id.rcvList)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.rcvList)).check(matches((isDisplayed())))
+        onView(withId(R.id.rcvList)).check(matches(hasChildCount(3)))
     }
 }
